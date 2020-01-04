cc01aea4c9cabb234fff3782c66b246f896292e2
baby <baby@example.com>
2020-01-04 14:39:18 +0800

    在时间线中显示时间

--- app/Post.php
+++ app/Post.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['user_id', 'body'];
+    protected $appends = ['createdDate']; //追加一个属性来获取时间
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    // 用法TBD
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
